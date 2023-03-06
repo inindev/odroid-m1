f8df6ab2a8d2a582c511a76312342e71523e8e80
John Clark <inindev@gmail.com>
2023-03-05 21:34:47 -0500

    linux 6.2.2
    Signed-off-by: John Clark <inindev@gmail.com>

--- dtb/make_dtb.sh
+++ dtb/make_dtb.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-# prerequisites: build-essential device-tree-compiler
-# kernel.org linux version
-
 main() {
-    local lv='6.2.1'
+    local lv='6.2.2'
+    local rkpath="linux-$lv/arch/arm64/boot/dts/rockchip"
 
     if [ '_clean' = "_$1" ]; then
         rm -f *.dt?
@@ -15,25 +13,24 @@ main() {
         exit 0
     fi
 
+    check_installed 'device-tree-compiler' 'gcc' 'wget' 'xz-utils'
+
     if [ ! -f "linux-$lv.tar.xz" ]; then
         wget "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$lv.tar.xz"
     fi
 
-    local lrcp="linux-$lv/arch/arm64/boot/dts/rockchip"
     if [ ! -d "linux-$lv" ]; then
-        tar "xavf" "linux-$lv.tar.xz" "linux-$lv/include/dt-bindings" "$lrcp"
+        tar "xavf" "linux-$lv.tar.xz" "linux-$lv/include/dt-bindings" "$rkpath"
     fi
 
     if [ '_links' = "_$1" ]; then
-        ln -sf "$lrcp/rk3568-odroid-m1.dts"
+        ln -sf "$rkpath/rk3568-odroid-m1.dts"
         echo '\nlink created\n'
         exit 0
     fi
 
-    check_installed 'device-tree-compiler' 'gcc' 'wget' 'xz-utils'
-
     # build
-    gcc -I "linux-$lv/include" -E -nostdinc -undef -D__DTS__ -x assembler-with-cpp -o rk3568-odroid-m1-top.dts "$lrcp/rk3568-odroid-m1.dts"
+    gcc -I "linux-$lv/include" -E -nostdinc -undef -D__DTS__ -x assembler-with-cpp -o rk3568-odroid-m1-top.dts "$rkpath/rk3568-odroid-m1.dts"
     dtc -@ -I dts -O dtb -o rk3568-odroid-m1.dtb rk3568-odroid-m1-top.dts
 
     echo '\nbuild complete: rk3568-odroid-m1.dtb\n'
