83ce4ac7f7f6028ae1bccbaa90d165fcdddcf026
John Clark <inindev@gmail.com>
2023-04-02 12:38:16 -0400

    use linux-firmware-20230210
    Signed-off-by: John Clark <inindev@gmail.com>

--- debian/make_debian_img.sh
+++ debian/make_debian_img.sh
@@ -14,24 +14,49 @@ main() {
     #   use 'm' for 1024^2 and 'g' for 1024^3
     local media='mmc_2g.img' # or block device '/dev/sdX'
     local deb_dist='bookworm'
-    local hostname='deb-arm64'
+    local hostname='odroidm1-arm64'
     local acct_uid='debian'
     local acct_pass='debian'
-    local disable_ipv6='true'
+    local disable_ipv6=true
     local part_uuid='0011732f-7182-416f-8962-d8b252435c47'
-    local extra_pkgs='pciutils, sudo, wget, u-boot-tools, xxd, xz-utils, zip, unzip'
+    local extra_pkgs='curl, pciutils, sudo, u-boot-tools, unzip, wget, xxd, xz-utils, zip, zstd'
+
+    is_param 'clean' $@ && rm -rf cache.* && rm mmc_2g.img* && exit 0
+
+    if [ -f "$media" ]; then
+        read -p "file $media exists, overwrite? <y/N> " yn
+        if ! [ "$yn" = 'y' -o "$yn" = 'Y' -o "$yn" = 'yes' -o "$yn" = 'Yes' ]; then
+            echo 'exiting...'
+            exit 0
+        fi
+    fi
 
     # no compression if disabled or block media
-    local compress=$([ "nocomp" = "$1" -o -b "$media" ] && echo false || echo true)
+    local compress=$(is_param 'nocomp' $@ || [ -b "$media" ] && echo false || echo true)
+
+    if $compress && [ -f "$media.xz" ]; then
+        read -p "file $media.xz exists, overwrite? <y/N> " yn
+        if ! [ "$yn" = 'y' -o "$yn" = 'Y' -o "$yn" = 'yes' -o "$yn" = 'Yes' ]; then
+            echo 'exiting...'
+            exit 0
+        fi
+    fi
 
     check_installed 'debootstrap' 'u-boot-tools' 'wget' 'xz-utils'
 
     print_hdr "downloading files"
     local cache="cache.$deb_dist"
-
+    # linux firmware
+    local lfw=$(download "$cache" 'https://mirrors.edge.kernel.org/pub/linux/kernel/firmware/linux-firmware-20230210.tar.xz')
+    local lfwsha='6e3d9e8d52cffc4ec0dbe8533a8445328e0524a20f159a5b61c2706f983ce38a'
     # device tree
-    local dtb=$(download "$cache" 'https://github.com/inindev/odroid-m1/releases/download/v12.0-rc2/rk3568-odroid-m1.dtb')
+    local dtb=$(download "$cache" 'https://github.com/inindev/odroid-m1/releases/download/v12.0-rc3/rk3568-odroid-m1.dtb')
 #    local dtb='../dtb/rk3568-odroid-m1.dtb'
+
+    if [ "$lfwsha" != $(sha256sum "$lfw" | cut -c1-64) ]; then
+        echo "invalid hash for linux firmware: $lfw"
+        exit 5
+    fi
 
     if [ ! -f "$dtb" ]; then
         echo "device tree binary is missing: $dtb"
@@ -49,7 +74,7 @@ main() {
     print_hdr "formatting media"
     format_media "$media"
 
-    mount_media "$media" "$mountpt"
+    mount_media "$media"
 
     # do not write the cache to the image
     mkdir -p "$cache/var/cache" "$cache/var/lib/apt/lists"
@@ -73,6 +98,10 @@ main() {
     echo "$(file_apt_sources $deb_dist)\n" > "$mountpt/etc/apt/sources.list"
     echo "$(file_locale_cfg)\n" > "$mountpt/etc/default/locale"
 
+    # disable sshd until after keys are regenerated on first boot
+    rm -f "$mountpt/etc/systemd/system/sshd.service"
+    rm -f "$mountpt/etc/systemd/system/multi-user.target.wants/ssh.service"
+
     rm -rf "$mountpt/etc/systemd/system/multi-user.target.wants/wpa_supplicant.service"
     echo "$(file_wpa_supplicant_conf)\n" > "$mountpt/etc/wpa_supplicant/wpa_supplicant.conf"
     cp "$mountpt/usr/share/dhcpcd/hooks/10-wpa_supplicant" "$mountpt/usr/lib/dhcpcd/dhcpcd-hooks"
@@ -87,6 +116,9 @@ main() {
     sed -i '/eval.*dircolors/s/^#*\s*//' "$mountpt/root/.bashrc"
     sed -i '/alias.l.=/s/^#*\s*//' "$mountpt/root/.bashrc"
 
+    # motd (off by default)
+    is_param 'motd' $@ && [ -f '../etc/motd' ] && cp -f '../etc/motd' "$mountpt/etc/motd"
+
     # setup /boot
     echo "$(script_boot_txt $part_uuid $disable_ipv6)\n" > "$mountpt/boot/boot.txt"
     mkimage -A arm64 -O linux -T script -C none -n 'u-boot boot script' -d "$mountpt/boot/boot.txt" "$mountpt/boot/boot.scr"
@@ -95,25 +127,27 @@ main() {
     install -m 644 "$dtb" "$mountpt/boot"
     ln -sf $(basename "$dtb") "$mountpt/boot/dtb"
 
+    print_hdr "installing firmware"
+    mkdir -p "$mountpt/lib/firmware"
+    local lfwn=$(basename "$lfw")
+    tar -C "$mountpt/lib/firmware" --strip-components=1 --wildcards -xavf "$lfw" "${lfwn%%.*}/rockchip" "${lfwn%%.*}/rtl_bt" "${lfwn%%.*}/rtl_nic"
+
+    print_hdr "installing rootfs expansion script to /etc/rc.local"
+    echo "$(script_rc_local)\n" > "$mountpt/etc/rc.local"
+    chmod 754 "$mountpt/etc/rc.local"
+
     print_hdr "creating user account"
     chroot "$mountpt" /usr/sbin/useradd -m $acct_uid -s /bin/bash
     chroot "$mountpt" /bin/sh -c "/usr/bin/echo $acct_uid:$acct_pass | /usr/sbin/chpasswd -c YESCRYPT"
     chroot "$mountpt" /usr/bin/passwd -e $acct_uid
     (umask 377 && echo "$acct_uid ALL=(ALL) NOPASSWD: ALL" > "$mountpt/etc/sudoers.d/$acct_uid")
 
-    # extra setup for non-block media
-    if [ ! -b "$media" ]; then
-        print_hdr "installing rootfs expansion script to /etc/rc.local"
-        echo "$(script_rc_local)\n" > "$mountpt/etc/rc.local"
-        chmod 754 "$mountpt/etc/rc.local"
-
-        # reduce entropy in free space to enhance compression
-        if $compress; then
-            print_hdr "removing entropy before compression"
-            cat /dev/zero > "$mountpt/tmp/zero.bin" 2> /dev/null || true
-            sync
-            rm -f "$mountpt/tmp/zero.bin"
-        fi
+    # when compressing, reduce entropy in free space to enhance compression
+    if $compress; then
+        print_hdr "removing entropy before compression"
+        cat /dev/zero > "$mountpt/tmp/zero.bin" 2> /dev/null || true
+        sync
+        rm -f "$mountpt/tmp/zero.bin"
     fi
 
     umount "$mountpt"
@@ -137,7 +171,7 @@ main() {
 
 make_image_file() {
     local filename="$1"
-    rm -f "$filename"
+    rm -f "$filename"*
     local size="$(echo "$filename" | sed -rn 's/.*mmc_([[:digit:]]+[m|g])\.img$/\1/p')"
     local bytes="$(echo "$size" | sed -e 's/g/ << 30/' -e 's/m/ << 20/')"
     dd bs=64K count=$(($bytes >> 16)) if=/dev/zero of="$filename" status=progress
@@ -163,40 +197,95 @@ format_media() {
     # create ext4 filesystem
     if [ -b "$media" ]; then
         local part1="/dev/$(lsblk -no kname "$media" | grep '.*p1$')"
-        mkfs.ext4 "$part1"
-        sync
+        mkfs.ext4 "$part1" && sync
     else
         local lodev="$(losetup -f)"
-        losetup -P "$lodev" "$media"
-        sync
-        mkfs.ext4 "${lodev}p1"
-        sync
-        losetup -d "$lodev"
-        sync
+        losetup -P "$lodev" "$media" && sync
+        mkfs.ext4 "${lodev}p1" && sync
+        losetup -d "$lodev" && sync
     fi
 }
 
 mount_media() {
     local media="$1"
-    local mountpoint="$2"
 
-    if [ -d "$mountpoint" ]; then
+    if [ -d "$mountpt" ]; then
+        echo "cleaning up mount points..."
+        mountpoint -q "$mountpt/var/cache" && umount "$mountpt/var/cache"
+        mountpoint -q "$mountpt/var/lib/apt/lists" && umount "$mountpt/var/lib/apt/lists"
         mountpoint -q "$mountpt" && umount "$mountpt"
     else
-        mkdir -p "$mountpoint"
+        mkdir -p "$mountpt"
     fi
 
     if [ -b "$media" ]; then
         local part1="/dev/$(lsblk -no kname "$media" | grep '.*p1$')"
-        mount -n "$part1" "$mountpoint"
+        mount -n "$part1" "$mountpt"
+    elif [ -f "$media" ]; then
+        mount -n -o loop,offset=16M "$media" "$mountpt"
     else
-        mount -n -o loop,offset=16M "$media" "$mountpoint"
+        echo "file not found: $media"
+        exit 4
     fi
 
-    if [ ! -d "$mountpoint/lost+found" ]; then
+    if [ ! -d "$mountpt/lost+found" ]; then
         echo 'failed to mount the image file'
         exit 3
     fi
+
+    echo "media ${cya}$media${rst} successfully mounted on ${cya}$mountpt${rst}"
+}
+
+check_mount_only() {
+    local img
+    local flag=false
+    for item in "$@"; do
+        case "$item" in
+            mount) flag=true ;;
+            *.img) img=$item ;;
+            *.img.xz) img=$item ;;
+        esac
+    done
+    ! $flag && return
+
+    if [ ! -f "$img" ]; then
+        if [ -z "$img" ]; then
+            echo "no image file specified"
+        else
+            echo "file not found: ${red}$img${rst}"
+        fi
+        exit 3
+    fi
+
+    if [ "$img" = *.xz ]; then
+        tmp=$(basename "$img" .xz)
+        if [ -f "$tmp" ]; then
+            echo "compressed file ${bld}$img${rst} was specified but uncompressed file ${bld}$tmp${rst} exists..."
+            echo -n "mount ${bld}$tmp${rst}"
+            read -p " instead? <Y/n> " yn
+            if ! [ -z "$yn" -o "$yn" = 'y' -o "$yn" = 'Y' -o "$yn" = 'yes' -o "$yn" = 'Yes' ]; then
+                echo 'exiting...'
+                exit 0
+            fi
+            img=$tmp
+        else
+            echo -n "compressed file ${bld}$img${rst} was specified"
+            read -p ', decompress to mount? <Y/n>' yn
+            if ! [ -z "$yn" -o "$yn" = 'y' -o "$yn" = 'Y' -o "$yn" = 'yes' -o "$yn" = 'Yes' ]; then
+                echo 'exiting...'
+                exit 0
+            fi
+            xz -dk "$img"
+            img=$(basename "$img" .xz)
+        fi
+    fi
+
+    echo "mounting file ${yel}$img${rst}..."
+    mount_media "$img"
+    trap - EXIT INT QUIT ABRT TERM
+    echo "media mounted, use ${grn}sudo umount $mountpt${rst} to unmount"
+
+    exit 0
 }
 
 # download / return file from cache
@@ -290,6 +379,7 @@ script_rc_local() {
 	    # regen ssh keys
 	    rm -f /etc/ssh/ssh_host_*
 	    dpkg-reconfigure openssh-server
+            systemctl enable ssh.service
 
 	    # expand root parition
 	    rp=\$(findmnt / -o source -n)
@@ -315,6 +405,8 @@ script_boot_txt() {
 
 	if load \${devtype} \${devnum}:\${partition} \${kernel_addr_r} /boot/vmlinuz; then
 	    if load \${devtype} \${devnum}:\${partition} \${fdt_addr_r} /boot/dtb; then
+	        fdt addr \${fdt_addr_r}
+	        fdt resize
 	        if load \${devtype} \${devnum}:\${partition} \${ramdisk_addr_r} /boot/initrd.img; then
 	            booti \${kernel_addr_r} \${ramdisk_addr_r}:\${filesize} \${fdt_addr_r};
 	        else
@@ -339,6 +431,18 @@ script_mkscr_sh() {
 	EOF
 }
 
+is_param() {
+    local match
+    for item in $@; do
+        if [ -z $match ]; then
+            match=$item
+        elif [ $match = $item ]; then
+            return
+        fi
+    done
+    false
+}
+
 print_hdr() {
     local msg=$1
     echo "\n${h1}$msg...${rst}"
@@ -352,10 +456,11 @@ on_exit() {
         mountpoint -q "$mountpt/var/lib/apt/lists" && umount "$mountpt/var/lib/apt/lists"
 
         read -p "$mountpt is still mounted, unmount? <Y/n> " yn
-        if [ "$yn" = "" -o "$yn" = "y" -o "$yn" = "Y" -o "$yn" = "yes" -o "$yn" = "Yes" ]; then
+        if [ -z "$yn" -o "$yn" = 'y' -o "$yn" = 'Y' -o "$yn" = 'yes' -o "$yn" = 'Yes' ]; then
             echo "unmounting $mountpt"
             umount "$mountpt"
             sync
+            rm -rf "$mountpt"
         fi
     fi
 }
@@ -374,7 +479,11 @@ h1="${blu}==>${rst} ${bld}"
 
 if [ 0 -ne $(id -u) ]; then
     echo 'this script must be run as root'
+    echo "   run: ${bld}${grn}sudo sh make_debian_img.sh${rst}\n"
     exit 9
 fi
 
+cd "$(dirname "$(readlink -f "$0")")"
+check_mount_only $@
 main $@
+
