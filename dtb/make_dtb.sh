da0c817ae9faa574b0a5e1502ef176261ef83d32
John Clark <inindev@gmail.com>
2023-04-02 11:21:06 -0400

    update to linux 6.2.9
    Signed-off-by: John Clark <inindev@gmail.com>

--- dtb/make_dtb.sh
+++ dtb/make_dtb.sh
@@ -2,9 +2,16 @@
 
 set -e
 
+# script exit codes:
+#   1: missing utility
+#   5: invalid file hash
+
 main() {
-    local lv='6.2.2'
-    local rkpath="linux-$lv/arch/arm64/boot/dts/rockchip"
+    local linux='https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.9.tar.xz'
+    local lxsha='903449c164c03f0e742aacc920e18563585e07a28c6cb79e0fd6c36695fd43f5'
+
+    local lf=$(basename $linux)
+    local lv=$(echo $lf | sed -nE 's/linux-(.*)\.tar\..z/\1/p')
 
     if [ '_clean' = "_$1" ]; then
         rm -f *.dt*
@@ -15,30 +22,34 @@ main() {
 
     check_installed 'device-tree-compiler' 'gcc' 'wget' 'xz-utils'
 
-    if [ ! -f "linux-$lv.tar.xz" ]; then
-        wget "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$lv.tar.xz"
+    [ -f $lf ] || wget $linux
+
+    if [ $lxsha != $(sha256sum $lf | cut -c1-64) ]; then
+        echo "invalid hash for linux source file: $lf"
+        exit 5
     fi
 
+    local rkpath=linux-$lv/arch/arm64/boot/dts/rockchip
     if [ ! -d "linux-$lv" ]; then
-        tar "xavf" "linux-$lv.tar.xz" "linux-$lv/include/dt-bindings" "$rkpath"
+        tar xavf $lf linux-$lv/include/dt-bindings linux-$lv/include/uapi $rkpath
     fi
 
     if [ '_links' = "_$1" ]; then
-        ln -sf "$rkpath/rk3568-odroid-m1.dts"
-        ln -sf "$rkpath/rk3568.dtsi"
-        ln -sf "$rkpath/rk356x.dtsi"
+        ln -sfv $rkpath/rk3568-pinctrl.dtsi
+        ln -sfv $rkpath/rk356x.dtsi
+        ln -sfv $rkpath/rk3568.dtsi
+        ln -sfv $rkpath/rk3568-odroid-m1.dts
         echo '\nlinks created\n'
         exit 0
     fi
 
     # build
-    gcc -I "linux-$lv/include" -E -nostdinc -undef -D__DTS__ -x assembler-with-cpp -o rk3568-odroid-m1-top.dts "$rkpath/rk3568-odroid-m1.dts"
-    dtc -@ -I dts -O dtb -o rk3568-odroid-m1.dtb rk3568-odroid-m1-top.dts
-
-    echo '\n${cya}build complete: rk3568-odroid-m1.dtb${rst}\n'
+    local dt=rk3568-odroid-m1
+    gcc -I linux-$lv/include -E -nostdinc -undef -D__DTS__ -x assembler-with-cpp -o ${dt}-top.dts $rkpath/${dt}.dts
+    dtc -@ -I dts -O dtb -o ${dt}.dtb ${dt}-top.dts
+    echo "\n${cya}device tree ready: ${dt}.dtb${rst}\n"
 }
 
-# check if utility program is installed
 check_installed() {
     local todo
     for item in "$@"; do
