d117b954c42ad5d97932b36a7731f81ae4cdb5fe
John Clark <inindev@gmail.com>
2023-06-01 19:18:33 -0400

    update to linux 6.3.5
    Signed-off-by: John Clark <inindev@gmail.com>

--- dtb/make_dtb.sh
+++ dtb/make_dtb.sh
@@ -7,8 +7,8 @@ set -e
 #   5: invalid file hash
 
 main() {
-    local linux='https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.3.3.tar.xz'
-    local lxsha='8975216a6cee827390586758ed69d19743367098d1fc5dd5694987bb529e44e5'
+    local linux='https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.3.5.tar.xz'
+    local lxsha='f5cd478c3d8b908ab606afd1e95a4f8f77e7186b4a82829251d6e6aaafff825e'
 
     local lf=$(basename $linux)
     local lv=$(echo $lf | sed -nE 's/linux-(.*)\.tar\..z/\1/p')
