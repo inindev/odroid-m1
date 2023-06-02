519a0b9515afd684bd8b4d03d8282e870adeeab5
John Clark <inindev@gmail.com>
2023-06-01 22:19:05 -0400

    add package libpam-systemd
    Signed-off-by: John Clark <inindev@gmail.com>

--- debian/make_debian_img.sh
+++ debian/make_debian_img.sh
@@ -99,7 +99,7 @@ main() {
     print_hdr "installing root filesystem from debian.org"
     mkdir "$mountpt/etc"
     echo 'link_in_boot = 1' > "$mountpt/etc/kernel-img.conf"
-    local pkgs="linux-image-arm64, dbus, dhcpcd5, openssh-server, systemd-timesyncd"
+    local pkgs="linux-image-arm64, dbus, dhcpcd5, libpam-systemd, openssh-server, systemd-timesyncd"
     pkgs="$pkgs, wireless-regdb, wpasupplicant"
     pkgs="$pkgs, $extra_pkgs"
     debootstrap --arch arm64 --include "$pkgs" --exclude "isc-dhcp-client" "$deb_dist" "$mountpt" 'https://deb.debian.org/debian/'
