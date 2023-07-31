afdc369c8706ca61fb0cc68a5e2402cd1c88b249
John Clark <inindev@gmail.com>
2023-07-31 00:18:18 +0000

    update to linux 6.4.7
    Signed-off-by: John Clark <inindev@gmail.com>

--- dtb/make_dtb.sh
+++ dtb/make_dtb.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Copyright (C) 2023, John Clark <inindev@gmail.com>
+
 set -e
 
 # script exit codes:
@@ -7,14 +9,15 @@ set -e
 #   5: invalid file hash
 
 main() {
-    local linux='https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.4.3.tar.xz'
-    local lxsha='7134ed29360df6f37a26410630283f0592c91a6d2178a9648226d30ddf8c88a1'
+    local linux='https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.4.7.tar.xz'
+    local lxsha='de143cb61dcaa756c05f56ff35144316d810615819518a33e34754f064c4a7d8'
 
     local lf="$(basename "$linux")"
     local lv="$(echo "$lf" | sed -nE 's/linux-(.*)\.tar\..z/\1/p')"
 
-    if [ '_clean' = "_$1" ]; then
-        rm -f *.dt*
+    if is_param 'clean' "$@"; then
+        rm -f *.dtb *-top.dts
+        find . -maxdepth 1 -type l -delete
         rm -rf "linux-$lv"
         echo '\nclean complete\n'
         exit 0
@@ -24,18 +27,23 @@ main() {
 
     [ -f "$lf" ] || wget "$linux"
 
-    if [ "_$lxsha" != _$(sha256sum "$lf" | cut -c1-64) ]; then
+    if [ "_$lxsha" != "_$(sha256sum "$lf" | cut -c1-64)" ]; then
         echo "invalid hash for linux source file: $lf"
         exit 5
     fi
 
     local rkpath="linux-$lv/arch/arm64/boot/dts/rockchip"
-    if [ ! -d "linux-$lv" ]; then
+    if ! [ -d "linux-$lv" ]; then
         tar xavf "$lf" "linux-$lv/include/dt-bindings" "linux-$lv/include/uapi" "$rkpath"
+
+        local patch patches="$(find patches -maxdepth 1 -name '*.patch' 2>/dev/null || true)"
+        for patch in $patches; do
+            patch -p1 -d "linux-$lv" -i "../$patch"
+        done
     fi
 
-    local rkfl='rk3568-pinctrl.dtsi rk356x.dtsi rk3568.dtsi rk3568-odroid-m1.dts rockchip-pinconf.dtsi'
-    if [ '_links' = "_$1" ]; then
+    if is_param 'links' "$@"; then
+        local rkf rkfl='rk3568-odroid-m1.dts rk3568.dtsi rk356x.dtsi rk3568-pinctrl.dtsi rockchip-pinconf.dtsi'
         for rkf in $rkfl; do
             ln -sfv "$rkpath/$rkf"
         done
@@ -44,17 +52,41 @@ main() {
     fi
 
     # build
-    local dt=rk3568-odroid-m1
-    gcc -I "linux-$lv/include" -E -nostdinc -undef -D__DTS__ -x assembler-with-cpp -o "${dt}-top.dts" "$rkpath/${dt}.dts"
-
+    local dt dts='rk3568-odroid-m1'
     local fldtc='-Wno-interrupt_provider -Wno-unique_unit_address -Wno-unit_address_vs_reg -Wno-avoid_unnecessary_addr_size -Wno-alias_paths -Wno-graph_child_address -Wno-simple_bus_reg'
-    dtc -I dts -O dtb -b 0 ${fldtc} -o "${dt}.dtb" "${dt}-top.dts"
-
-    echo "\n${cya}device tree ready: ${dt}.dtb${rst}\n"
+    for dt in $dts; do
+        gcc -I "linux-$lv/include" -E -nostdinc -undef -D__DTS__ -x assembler-with-cpp -o "${dt}-top.dts" "$rkpath/${dt}.dts"
+        dtc -I dts -O dtb -b 0 ${fldtc} -o "${dt}.dtb" "${dt}-top.dts"
+        is_param 'cp' "$@" && cp_to_debian "${dt}.dtb"
+        echo "\n${cya}device tree ready: ${dt}.dtb${rst}\n"
+    done
 }
 
+cp_to_debian() {
+    local target="$1"
+    local deb_dist=$(cat "../debian/make_debian_img.sh" | sed -n 's/\s*local deb_dist=.\([[:alpha:]]\+\)./\1/p')
+    [ -z "$deb_dist" ] && return
+    local cdir="../debian/cache.$deb_dist"
+    echo '\ncopying to debian cache...'
+    sudo mkdir -p "$cdir"
+    sudo cp -v "$target" "$cdir"
+}
+
+is_param() {
+    local item match
+    for item in "$@"; do
+        if [ -z "$match" ]; then
+            match="$item"
+        elif [ "$match" = "$item" ]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+# check if debian package is installed
 check_installed() {
-    local todo
+    local item todo
     for item in "$@"; do
         dpkg -l "$item" 2>/dev/null | grep -q "ii  $item" || todo="$todo $item"
     done
@@ -64,6 +96,11 @@ check_installed() {
         echo "   run: ${bld}${grn}sudo apt update && sudo apt -y install$todo${rst}\n"
         exit 1
     fi
+}
+
+print_hdr() {
+    local msg="$1"
+    echo "\n${h1}$msg...${rst}"
 }
 
 rst='\033[m'
@@ -76,5 +113,6 @@ mag='\033[35m'
 cya='\033[36m'
 h1="${blu}==>${rst} ${bld}"
 
+cd "$(dirname "$(readlink -f "$0")")"
 main "$@"
 
